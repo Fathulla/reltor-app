1b9009b2e5cd54ce6a2d175722e6e56b09f2146f
Eva Corp <fathullarasidov239@gmail.com>
2023-10-26 22:03:57 +0500

    Update baseQuery.ts

--- src/utils/baseQuery.ts
+++ src/utils/baseQuery.ts
@@ -1,4 +1,3 @@
-export const baseQuery = 'https://realtor16.p.rapidapi.com'
-export const xHost = 'realtor16.p.rapidapi.com'
-export const xKey = '92ce4ff40amsh3464f2a50db9883p1d679cjsn0ff65afd359f'
-// export const xKey = 'a912be4f5fmshb1bf0e8759d998ep1b62adjsn27888c50fcc5'
+export const baseQuery = "https://realtor16.p.rapidapi.com";
+export const xHost = "realtor16.p.rapidapi.com";
+export const xKey = "c674703fb0mshb75f3e854091f2fp18fccejsnb6749216e8d8";
